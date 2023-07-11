5713c5b5629b4a93055c3a219ee1ebf704a0d49d
firefather <firefather@telenet.be>
2023-07-12 01:32:41 +0200

    Update misc.cpp

--- src/nnue/misc.cpp
+++ src/nnue/misc.cpp
@@ -1,30 +1,23 @@
 /*
   Fire is a freeware UCI chess playing engine authored by Norman Schmidt.
-
   Fire utilizes many state-of-the-art chess programming ideas and techniques
   which have been documented in detail at https://www.chessprogramming.org/
   and demonstrated via the very strong open-source chess engine Stockfish...
   https://github.com/official-stockfish/Stockfish.
-
   Fire is free software: you can redistribute it and/or modify it under the
   terms of the GNU General Public License as published by the Free Software
   Foundation, either version 3 of the License, or any later version.
-
   You should have received a copy of the GNU General Public License with
   this program: copying.txt.  If not, see <http://www.gnu.org/licenses/>.
-
   Thanks to Yu Nasu, Hisayori Noda. This implementation adapted from R. De Man
   and Daniel Shaw's Cfish nnue probe code https://github.com/dshawul/nnue-probe
 */
-
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <cstring>
 #include <cstdio>
 #include <cctype>
-
 #include "misc.h"
-
 FD open_file(const char* name)
 {
 #ifndef _WIN32
@@ -34,7 +27,6 @@ FD open_file(const char* name)
 	                  FILE_FLAG_RANDOM_ACCESS, nullptr);
 #endif
 }
-
 void close_file(const FD fd)
 {
 #ifndef _WIN32
@@ -43,7 +35,6 @@ void close_file(const FD fd)
 	CloseHandle(fd);
 #endif
 }
-
 size_t file_size(const FD fd)
 {
 #ifndef _WIN32
@@ -56,7 +47,6 @@ size_t file_size(const FD fd)
 	return (static_cast<uint64_t>(size_high) << 32) | size_low;
 #endif
 }
-
 const void* map_file(const FD fd, map_t* map)
 {
 #ifndef _WIN32
@@ -66,21 +56,17 @@ const void* map_file(const FD fd, map_t* map)
 	madvise(data, *map, MADV_RANDOM);
 #endif
 	return data == MAP_FAILED ? NULL : data;
-
 #else
 	DWORD size_high;
 	const DWORD size_low = GetFileSize(fd, &size_high);
 	*map = CreateFileMapping(fd, nullptr, PAGE_READONLY, size_high, size_low, nullptr);
-	if (*map == nullptr)
-		return nullptr;
+	if (*map == nullptr) return nullptr;
 	return MapViewOfFile(*map, FILE_MAP_READ, 0, 0, 0);
 #endif
 }
-
 void unmap_file(const void* data, map_t map)
 {
 	if (!data) return;
-
 #ifndef _WIN32
 	munmap((void*)data, map);
 #else
