478aa4ba33bed6501d97d27e479511440e8b1538
firefather <firefather@telenet.be>
2023-07-12 01:37:25 +0200

    Update pragma.h

--- src/pragma.h
+++ src/pragma.h
@@ -1,19 +1,15 @@
 /*
   Fire is a freeware UCI chess playing engine authored by Norman Schmidt.
-
   Fire utilizes many state-of-the-art chess programming ideas and techniques
   which have been documented in detail at https://www.chessprogramming.org/
   and demonstrated via the very strong open-source chess engine Stockfish...
   https://github.com/official-stockfish/Stockfish.
-
   Fire is free software: you can redistribute it and/or modify it under the
   terms of the GNU General Public License as published by the Free Software
   Foundation, either version 3 of the License, or any later version.
-
   You should have received a copy of the GNU General Public License with
   this program: copying.txt.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 // disable specific compiler warnings
 #pragma once
 #ifdef _MSC_VER
@@ -24,7 +20,6 @@
 #else
 #pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
 #endif
-
 #ifdef __INTEL_COMPILER
 #pragma warning(disable: 1491) // definition from the enclosing scope function is used.
 #pragma warning(disable: 26812) // The enum type '' is unscoped. Prefer 'enum class' over 'enum'
