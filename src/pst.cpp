218f2746ecd750f955a3e6c2eb7a3780e6461cbf
firefather <firefather@telenet.be>
2023-07-12 01:37:58 +0200

    Update pst.cpp

--- src/pst.cpp
+++ src/pst.cpp
@@ -1,25 +1,19 @@
 /*
   Fire is a freeware UCI chess playing engine authored by Norman Schmidt.
-
   Fire utilizes many state-of-the-art chess programming ideas and techniques
   which have been documented in detail at https://www.chessprogramming.org/
   and demonstrated via the very strong open-source chess engine Stockfish...
   https://github.com/official-stockfish/Stockfish.
-
   Fire is free software: you can redistribute it and/or modify it under the
   terms of the GNU General Public License as published by the Free Software
   Foundation, either version 3 of the License, or any later version.
-
   You should have received a copy of the GNU General Public License with
   this program: copying.txt.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 #include "fire.h"
 #include "position.h"
-
 // piece sq tables assign an value to each piece depending on which sq it's located
-namespace pst
-{
+namespace pst {
 	int psq[num_pieces][num_squares] =
 	{
 		{
