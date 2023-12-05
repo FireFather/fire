25b687fa9bbd7fa177d811748be6bd474c8413cc
Kato-Daichi <firefather@telenet.be>
2023-12-05 04:52:15 +0100

    Update mutex.h

--- src/mutex.h
+++ src/mutex.h
@@ -1,29 +1,23 @@
-/*
-  Fire is a freeware UCI chess playing engine authored by Norman Schmidt.
-  Fire utilizes many state-of-the-art chess programming ideas and techniques
-  which have been documented in detail at https://www.chessprogramming.org/
-  and demonstrated via the very strong open-source chess engine Stockfish...
-  https://github.com/official-stockfish/Stockfish.
-  Fire is free software: you can redistribute it and/or modify it under the
-  terms of the GNU General Public License as published by the Free Software
-  Foundation, either version 3 of the License, or any later version.
-  You should have received a copy of the GNU General Public License with
-  this program: copying.txt.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #pragma once
 #include <condition_variable>
 #include <mutex>
+
 #if defined(_WIN32) && !defined(_MSC_VER)
 #include <windows.h>
-struct Mutex
-{
-	Mutex() { InitializeCriticalSection(&cs); }
-	~Mutex() { DeleteCriticalSection(&cs); }
-	void lock() { EnterCriticalSection(&cs); }
-	void unlock() { LeaveCriticalSection(&cs); }
-private:
-	CRITICAL_SECTION cs;
+
+struct Mutex {
+  Mutex() { InitializeCriticalSection(&cs); }
+
+  ~Mutex() { DeleteCriticalSection(&cs); }
+
+  void lock() { EnterCriticalSection(&cs); }
+
+  void unlock() { LeaveCriticalSection(&cs); }
+
+ private:
+  CRITICAL_SECTION cs;
 };
+
 typedef std::condition_variable_any ConditionVariable;
 #else
 using Mutex = std::mutex;
