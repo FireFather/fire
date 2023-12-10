88c92434362b7e9acc659db7cd20c6c6e13f8c4a
Kato-Daichi <firefather@telenet.be>
2023-12-11 00:22:39 +0100

    update shell scripts and project files

--- src/make_all.sh
+++ src/make_all.sh
@@ -1,32 +1,11 @@
-#!/bin/bash
-# make_all.sh
-
-# Fire is a freeware UCI chess playing engine authored by Norman Schmidt.
-#  
-# Fire is free software: you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or any later version.
-# 
-# You should have received a copy of the GNU General Public License with
-# this program: copying.txt.  If not, see <http://www.gnu.org/licenses/>.
-
-# cpu architecture set to x86 64-bit w/ Intel's avx2 (Advanced Vector Extensions 2)
-# also known as Haswell New Instructions
-
-arch_cpu=x86-64-popc
+arch_cpu=x86-64-pext
 make --no-print-directory -j profile-build ARCH=${arch_cpu} COMP=mingw
 strip fire.exe
-mv fire.exe fire-9.2_x64_popc.exe
-make gcc-profile-clean 
+mv fire.exe fire-9.2_x64_pext.exe 
+make gcc-profile-clean
 
 arch_cpu=x86-64-avx2
 make --no-print-directory -j profile-build ARCH=${arch_cpu} COMP=mingw
 strip fire.exe
 mv fire.exe fire-9.2_x64_avx2.exe 
-make gcc-profile-clean
-
-arch_cpu=x86-64-bmi2
-make --no-print-directory -j profile-build ARCH=${arch_cpu} COMP=mingw
-strip fire.exe
-mv fire.exe fire-9.2_x64_bmi2.exe 
 make gcc-profile-clean
