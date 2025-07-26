109f69fb7fbf3ac828c869f2c8b1f9c69d6ec556
firefather <firefather@telenet.be>
2025-07-27 00:11:25 +0200

    update chrono

--- src/chrono.cpp
+++ src/chrono.cpp
@@ -3,68 +3,59 @@
 #include "main.h"
 #include "uci.h"
 
-void timecontrol::init(const search_param& limit, const side me,
-  const int ply) {
-  start_time_ = now();
-  optimal_time_ = maximum_time_ = limit.time[me];
-
-  const auto maxmoves = limit.moves_to_go
-    ? std::min(limit.moves_to_go, moves_horizon_)
-    : moves_horizon_;
-  const auto move_importance =
-    calc_move_importance(ply) * move_importance_factor_;
-  double other_moves_importance = 0;
-
-  auto available = static_cast<int64_t>(limit.time[me]) -
+void timecontrol::init(const search_param& limit,const side me,
+  const int ply){
+  start_time_=now();
+  optimal_time_=maximum_time_=limit.time[me];
+  const auto maxmoves=limit.moves_to_go
+    ?std::min(limit.moves_to_go,moves_horizon_)
+    :moves_horizon_;
+  const auto move_importance=
+    calc_move_importance(ply)*move_importance_factor_;
+  double other_moves_importance=0;
+  auto available=static_cast<int64_t>(limit.time[me])-
     static_cast<int64_t>(move_overhead);
-
-  for (auto n = 1; n <= maxmoves; n++) {
-    const auto ratio1 =
-      move_importance / (move_importance + other_moves_importance);
-    int64_t t1 = std::llround(static_cast<double>(available) * ratio1);
-
-    auto ratio2 = max_ratio_ * move_importance /
-      (max_ratio_ * move_importance + other_moves_importance);
-    auto ratio3 = (move_importance + steal_ratio_ * other_moves_importance) /
-      (move_importance + other_moves_importance);
-    int64_t t2 =
-      std::llround(static_cast<double>(available) * std::min(ratio2, ratio3));
-
-    optimal_time_ = std::min(t1, optimal_time_);
-    maximum_time_ = std::min(t2, maximum_time_);
-
-    other_moves_importance += calc_move_importance(ply + 2 * n);
-    available += static_cast<int64_t>(limit.inc[me]) -
+  for(auto n=1;n<=maxmoves;n++){
+    const auto ratio1=
+      move_importance/(move_importance+other_moves_importance);
+    int64_t t1=std::llround(static_cast<double>(available)*ratio1);
+    auto ratio2=max_ratio_*move_importance/
+      (max_ratio_*move_importance+other_moves_importance);
+    auto ratio3=(move_importance+steal_ratio_*other_moves_importance)/
+      (move_importance+other_moves_importance);
+    int64_t t2=
+      std::llround(static_cast<double>(available)*std::min(ratio2,ratio3));
+    optimal_time_=std::min(t1,optimal_time_);
+    maximum_time_=std::min(t2,maximum_time_);
+    other_moves_importance+=calc_move_importance(ply+2*n);
+    available+=static_cast<int64_t>(limit.inc[me])-
       static_cast<int64_t>(move_overhead);
   }
-
-  optimal_time_ = std::max(optimal_time_, minimum_time);
-  maximum_time_ = std::max(maximum_time_, minimum_time);
-
-  if (uci_ponder) {
-    optimal_time_ += optimal_time_ * 3 / 10;
-    optimal_time_ = std::min(optimal_time_, maximum_time_);
+  optimal_time_=std::max(optimal_time_,minimum_time);
+  maximum_time_=std::max(maximum_time_,minimum_time);
+  if(uci_ponder){
+    optimal_time_+=optimal_time_*3/10;
+    optimal_time_=std::min(optimal_time_,maximum_time_);
   }
 }
 
-void timecontrol::adjustment_after_ponder_hit() {
-  const auto new_max_time = maximum_time_ + elapsed();
-  optimal_time_ = optimal_time_ * new_max_time / maximum_time_;
+void timecontrol::adjustment_after_ponder_hit(){
+  const auto new_max_time=maximum_time_+elapsed();
+  optimal_time_=optimal_time_*new_max_time/maximum_time_;
 }
 
-double timecontrol::calc_move_importance(const int ply) const {
-  auto factor = 1.0;
-  if (ply > ply_min_ && ply < ply_max_)
-    factor = factor_base_ -
-    ply_factor_ *
-    (static_cast<double>(ply) - static_cast<double>(base_moves_)) *
-    (static_cast<double>(ply) - static_cast<double>(base_moves_));
-
-  return factor * pow(1 + exp((ply - x_shift_) / x_scale_), -skew_);
+double timecontrol::calc_move_importance(const int ply) const{
+  auto factor=1.0;
+  if(ply>ply_min_&&ply<ply_max_)
+    factor=factor_base_-
+      ply_factor_*
+      (static_cast<double>(ply)-static_cast<double>(base_moves_))*
+      (static_cast<double>(ply)-static_cast<double>(base_moves_));
+  return factor*pow(1+exp((ply-x_shift_)/x_scale_),-skew_);
 }
 
-int64_t timecontrol::elapsed() const {
-  return now() - start_time_;
+int64_t timecontrol::elapsed() const{
+  return now()-start_time_;
 }
 
 timecontrol time_control;
--- src/chrono.h
+++ src/chrono.h
@@ -1,73 +1,72 @@
 ﻿#pragma once
 #include "main.h"
 
-using time_point = std::chrono::milliseconds::rep;
-using max_moves_list = movelist<max_moves>;
+using time_point=std::chrono::milliseconds::rep;
+using max_moves_list=movelist<max_moves>;
 
-inline time_point now() {
+inline time_point now(){
   return std::chrono::duration_cast<std::chrono::milliseconds>(
-    std::chrono::steady_clock::now().time_since_epoch())
+      std::chrono::steady_clock::now().time_since_epoch())
     .count();
 }
 
-struct search_param {
-  search_param()
-    : moves_to_go(0),
-    depth(0),
-    move_time(0),
-    mate(0),
-    infinite(0),
-    ponder(0),
-    nodes(time[white] = time[black] = inc[white] = inc[black] =
-    moves_to_go = depth = move_time = mate = infinite = ponder =
-    0) {}
+struct search_param{
+  search_param() : moves_to_go(0),
+  depth(0),
+  move_time(0),
+  mate(0),
+  infinite(0),
+  ponder(0),
+  nodes(time[white]=time[black]=inc[white]=inc[black]=
+    moves_to_go=depth=move_time=mate=infinite=ponder=
+    0){}
 
-  [[nodiscard]] bool use_time_calculating() const {
-    return !(mate | move_time | depth | nodes | infinite);
+  [[nodiscard]] bool use_time_calculating() const{
+    return !(mate|move_time|depth|nodes|infinite);
   }
 
-  int time[num_sides]{}, inc[num_sides]{}, moves_to_go, depth, move_time, mate,
-    infinite, ponder;
+  int time[num_sides]{},inc[num_sides]{},moves_to_go,depth,move_time,mate,
+    infinite,ponder;
   uint64_t nodes;
   max_moves_list search_moves;
-  time_point start_time = 0;
+  time_point start_time=0;
 };
 
-class timecontrol {
+class timecontrol{
 public:
-  void init(const search_param& limit, side me, int ply);
+  void init(const search_param& limit,side me,int ply);
 
-  [[nodiscard]] int64_t optimum() const {
+  [[nodiscard]] int64_t optimum() const{
     return optimal_time_;
   }
 
-  [[nodiscard]] int64_t maximum() const {
+  [[nodiscard]] int64_t maximum() const{
     return maximum_time_;
   }
 
   [[nodiscard]] int64_t elapsed() const;
   [[nodiscard]] double calc_move_importance(int ply) const;
   void adjustment_after_ponder_hit();
-  int move_overhead = 50;
+  int move_overhead=50;
 private:
-  time_point start_time_ = 0;
-  int64_t optimal_time_ = 0;
-  int64_t maximum_time_ = 0;
+  time_point start_time_=0;
+  int64_t optimal_time_=0;
+  int64_t maximum_time_=0;
 
-  double x_scale_ = 7.64;
-  double x_shift_ = 58.4;
-  double skew_ = 0.183;
-  double factor_base_ = 1.225;
-  double ply_factor_ = 0.00025;
-  int ply_min_ = 10;
-  int ply_max_ = 70;
-  int base_moves_ = 50;
+  double x_scale_=7.64;
+  double x_shift_=58.4;
+  double skew_=0.183;
+  double factor_base_=1.225;
+  double ply_factor_=0.00025;
+  int ply_min_=10;
+  int ply_max_=70;
+  int base_moves_=50;
 
-  double move_importance_factor_ = 0.89;
-  int moves_horizon_ = 50;
-  double max_ratio_ = 7.09;
-  double steal_ratio_ = 0.35;
-  int64_t minimum_time = 1;
+  double move_importance_factor_=0.89;
+  int moves_horizon_=50;
+  double max_ratio_=7.09;
+  double steal_ratio_=0.35;
+  int64_t minimum_time=1;
 };
 
 extern timecontrol time_control;
