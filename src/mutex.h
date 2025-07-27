d3badad363c1f9b111763fcbe8f2f2e93631b563
firefather <firefather@telenet.be>
2025-07-27 19:33:38 +0200

    Update mutex.h

--- src/mutex.h
+++ src/mutex.h
@@ -5,17 +5,17 @@
 #if defined(_WIN32) && !defined(_MSC_VER)
 #include <windows.h>
 
-struct Mutex {
-  Mutex() { InitializeCriticalSection(&cs); }
-  ~Mutex() { DeleteCriticalSection(&cs); }
+struct std_mutex {
+  std_mutex() { InitializeCriticalSection(&cs); }
+  ~std_mutex() { DeleteCriticalSection(&cs); }
   void lock() { EnterCriticalSection(&cs); }
   void unlock() { LeaveCriticalSection(&cs); }
 private:
   CRITICAL_SECTION cs;
 };
 
-typedef std::condition_variable_any ConditionVariable;
+typedef std::condition_variable_any cond_var;
 #else
-using Mutex = std::mutex;
-using ConditionVariable = std::condition_variable;
+using std_mutex=std::mutex;
+using cond_var=std::condition_variable;
 #endif
