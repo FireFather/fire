1f6c07613ed4f622e1eb0ca3d229ebb6ae6ac91b
firefather <firefather@telenet.be>
2025-08-09 10:48:05 +0200

    update eval.cpp
    add comments
    update binary

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1,73 +1,97 @@
+// evaluate.cpp
+// Contains functions to evaluate chess positions using NNUE (efficiently updatable neural network)
+// Requires: main.h, nnue.h, position.h
+
 #include "main.h"
 #include "nnue.h"
 #include "position.h"
 
-namespace evaluate{ namespace{
-    int eval_nnue(const position& pos){
-      int pieces[33]{};
-      int squares[33]{};
-      int index=2;
-      for(uint8_t i=0;i<64;i++){
-        if(pos.piece_on_square(static_cast<square>(i))==1){
-          pieces[0]=1;
-          squares[0]=i;
-        } else if(pos.piece_on_square(static_cast<square>(i))==9){
-          pieces[1]=7;
-          squares[1]=i;
-        } else if(pos.piece_on_square(static_cast<square>(i))==2){
-          pieces[index]=6;
-          squares[index]=i;
-          index++;
-        } else if(pos.piece_on_square(static_cast<square>(i))==3){
-          pieces[index]=5;
-          squares[index]=i;
-          index++;
-        } else if(pos.piece_on_square(static_cast<square>(i))==4){
-          pieces[index]=4;
-          squares[index]=i;
-          index++;
-        } else if(pos.piece_on_square(static_cast<square>(i))==5){
-          pieces[index]=3;
-          squares[index]=i;
-          index++;
-        } else if(pos.piece_on_square(static_cast<square>(i))==6){
-          pieces[index]=2;
-          squares[index]=i;
-          index++;
-        } else if(pos.piece_on_square(static_cast<square>(i))==10){
-          pieces[index]=12;
-          squares[index]=i;
-          index++;
-        } else if(pos.piece_on_square(static_cast<square>(i))==11){
-          pieces[index]=11;
-          squares[index]=i;
-          index++;
-        } else if(pos.piece_on_square(static_cast<square>(i))==12){
-          pieces[index]=10;
-          squares[index]=i;
-          index++;
-        } else if(pos.piece_on_square(static_cast<square>(i))==13){
-          pieces[index]=9;
-          squares[index]=i;
-          index++;
-        } else if(pos.piece_on_square(static_cast<square>(i))==14){
-          pieces[index]=8;
-          squares[index]=i;
-          index++;
+namespace evaluate {
+namespace {
+
+// eval_nnue: Extracts piece types and square indices from the position,
+// prepares arrays for the NNUE input, and calls the neural network evaluator.
+int eval_nnue(const position& pos) {
+    // Arrays to hold piece type codes and corresponding square indices.
+    // Max 33 pieces: index 0-1 reserved for kings, others start at index 2.
+    int pieces[33]{};
+    int squares[33]{};
+
+    int index = 2;  // start filling from index 2
+    // Iterate over all 64 squares
+    for (uint8_t i = 0; i < 64; i++) {
+        square sq = static_cast<square>(i);
+        int pc = pos.piece_on_square(sq);
+
+        // White king
+        if (pc == 1) {
+            pieces[0] = 1;  // code for white king
+            squares[0] = i;
         }
-      }
-      const int nnue_score=nnue_evaluate(pos.on_move(),pieces,squares);
-      return nnue_score;
+        // Black king
+        else if (pc == 9) {
+            pieces[1] = 7;  // code for black king
+            squares[1] = i;
+        }
+        // Other piece types: mapping pos code to NNUE code
+        else if (pc == 2) { // white queen
+            pieces[index] = 6;
+            squares[index] = i;
+            index++;
+        } else if (pc == 3) { // white rook
+            pieces[index] = 5;
+            squares[index] = i;
+            index++;
+        } else if (pc == 4) { // white bishop
+            pieces[index] = 4;
+            squares[index] = i;
+            index++;
+        } else if (pc == 5) { // white knight
+            pieces[index] = 3;
+            squares[index] = i;
+            index++;
+        } else if (pc == 6) { // white pawn
+            pieces[index] = 2;
+            squares[index] = i;
+            index++;
+        } else if (pc == 10) { // black queen
+            pieces[index] = 12;
+            squares[index] = i;
+            index++;
+        } else if (pc == 11) { // black rook
+            pieces[index] = 11;
+            squares[index] = i;
+            index++;
+        } else if (pc == 12) { // black bishop
+            pieces[index] = 10;
+            squares[index] = i;
+            index++;
+        } else if (pc == 13) { // black knight
+            pieces[index] = 9;
+            squares[index] = i;
+            index++;
+        } else if (pc == 14) { // black pawn
+            pieces[index] = 8;
+            squares[index] = i;
+            index++;
+        }
     }
-  }
 
-  int eval(const position& pos){
-    const int nnue_score=eval_nnue(pos);
+    // Call NNUE evaluator: on_move determines which side to evaluate for
+    const int nnue_score = nnue_evaluate(pos.on_move(), pieces, squares);
     return nnue_score;
-  }
-
-  int eval_after_null_move(const int eval){
-    const auto result=-eval;
-    return result;
-  }
 }
+
+} // anonymous namespace
+
+// Public API: Evaluate a position by computing the NNUE score.
+int eval(const position& pos) {
+    return eval_nnue(pos);
+}
+
+// Evaluate after a null move: simply negate the evaluation.
+int eval_after_null_move(const int eval) {
+    return -eval;
+}
+
+} // namespace evaluate
